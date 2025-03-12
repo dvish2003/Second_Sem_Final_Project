0555d7149150b1a7053938ee9e2a40923f824295
dvish2003 <Vishanchathuranga2003@gmail.com>
2025-03-12 18:23:11 +0530

    Login Page 50% done

---
+++ Back_End_PreRental/src/main/java/lk/ijse/back_end_prerental/BackEndPreRentalApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.back_end_prerental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPreRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndPreRentalApplication.class, args);
+    }
+
+}
---
+++ Back_End_PreRental/src/test/java/lk/ijse/back_end_prerental/BackEndPreRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.back_end_prerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPreRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
